96d2e85011086cb382dc1558fb5798365e09c283
Yurii Rochniak <yurii.rochniak@tubemogul.com>
2015-11-12 16:45:03 -0800

    Will add description later

--- check_file_age.py
+++ check_file_age.py
@@ -1,56 +1,66 @@
 import os, time, stat
 import argparse
 from glob import glob
+from itertools import chain
 
 UNKNOWN = 3
 CRITICAL = 2
 WARNING = 1
 OK = 0
 parser = argparse.ArgumentParser( )
+parser.add_argument("--directory",
+                     help="Serch path")
+parser.add_argument("--file",
+                     help="Search pattern")
+parser.add_argument("--log",
+                     default="/mnt/logs/outdated-files.log",
+                     help="Where to store outdated files")
 parser.add_argument("--warning",
-                    default=3600,
-                    help="threshold for warning alert e.g. 500")
+                    default=2592000,
+                    help="threshold for critical alert e.g. 1000")
 parser.add_argument("--critical",
-                    default=36000,
+                    default=5184000,
                     help="threshold for critical alert e.g. 1000")
 args = parser.parse_args( )
 
-def file_check(warning , critical):
+def file_check(warning , critical , directory , file_r):
+    if not directory:
+        directory = r"{}".format(os.path.dirname(os.path.realpath(__file__)))
+    pattern = r'{}'.format(file_r)
     chk_crit = [0]
     chk_warn = [0]
-    files = [y for x in os.walk("DIRECTORY") for y in glob(os.path.join(x[0], '*.py'))]
+    files = [y for x in os.walk(directory) for y in glob(os.path.join(x[0], pattern))]
     log = open('log.txt', 'w')
     for f in files:
         st = os.stat(f)
         mtime = st.st_mtime
-        age =  int(time.time() - mtime)
-        if age > critical:
-    #        print "CRITICAL: There are outdated files"
-             print >> log, f + " " + "is older than a threshold %s" % args.critical
-             chk_crit = chk_crit.extend(1)
-        elif age > warning:
-    #        print "WARNING: There are outdated files"
-             print >> log, f + " " + "is in warning state %s" % age >> log
-             chk_warn = chk_warn.extend(1)
-        else:
-    #        print "OK: There are no outdated files"
-             continue
+        age = int(time.time() - mtime)
+        print age
+        if age > int(critical):
+            print >> log, f + " " + "is older than a threshold %s" % args.critical
+            chk_crit.extend([1])
+        elif age > int(warning):
+            print >> log, f + " " + "is in warning state %s" % age
+            chk_warn.extend([1])
+
     if (max(chk_crit) == 1):
         res = 1
-    elif (max(chk_warn) == 1 and max(chk_crit) == 0):
+    elif (max(chk_warn) == 1):
         res = 0.5
-#    elif not (np.all(chk_crit) and np.all(chk_warn) and chk_ok == 1):
-#        res = 0
     else:
-#        print "Unknown error. Check script"
-#        exit(UNKNOWN)a
-         res = 0
+        res = 0
+    print pattern
+    print directory
+    print files
+    print warning
+    print critical
     print chk_crit
     print chk_warn
     return res
     log.close()
+
 def main():
-    res = file_check(args.warning,args.critical)
+    res = file_check(args.warning,args.critical,args.directory,args.file)
     if res == 1:
         print "CRITICAL: There are outdated files"
         exit(CRITICAL)
